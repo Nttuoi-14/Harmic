94bfbf01a866c020586bef4e6f87376979687a24
MyLatitude <MyLatitude@DESKTOP-A08CVB9>
2024-11-24 16:04:49 +0700

    Add project files.

---
+++ ViewComponents/BlogViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using ASP.NET_Core_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ASP.NET_Core_MVC.ViewComponents
+{
+    public class BlogViewComponent : ViewComponent
+    {
+        private readonly HarmicContext _context;
+        public BlogViewComponent(HarmicContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = _context.TbBlogs.Include(m => m.Category).Where(m => (bool)m.IsActive);
+            return await Task.FromResult<IViewComponentResult>(View(items.OrderByDescending(m => m.BlogId).ToList()));
+        }
+    }
+}
---
+++ ViewComponents/ProductViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using ASP.NET_Core_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NET_Core_MVC.Viewcomponents
+{
+    public class ProductViewComponent : ViewComponent
+    {
+        private readonly HarmicContext _context;
+
+        public ProductViewComponent(HarmicContext context)
+        {
+            _context = context;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = _context.TbProducts.Include(m => m.CategoryProduct)
+                .Where(m => (bool)m.IsActive).Where(m => m.IsNew);
+            return await Task.FromResult<IViewComponentResult>(View(items.OrderByDescending(m => m.ProductId).ToList()));
+        }
+    }
+}
